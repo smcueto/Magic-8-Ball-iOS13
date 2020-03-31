f3872cac72dccd4436a3bf3a513e5bf48c81b68f
Stephanie Cueto <stephaniem.cueto@gmail.com>
2020-03-31 16:14:20 -0700

    eightball challenge

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
-
-
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func askButtonPressed(_sender: UIButton){
+    
+    let ballArray = [#imageLiteral(resourceName: "ball1"),#imageLiteral(resourceName: "ball4"),#imageLiteral(resourceName: "ball3"),#imageLiteral(resourceName: "ball5"),#imageLiteral(resourceName: "ball5")]
+        imageView.image = ballArray[Int.random(in: 0...4)]
+    }
 
 }
 
